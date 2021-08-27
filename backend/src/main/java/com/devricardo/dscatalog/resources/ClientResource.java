a9542cab3fd76bec310169bc4e37361b949d1e0e
ricardoDavids <ricardo.david2020.new@gmail.com>
2021-08-27 22:48:13 +0100

    ClientResource

--- backend/src/main/java/com/devricardo/dscatalog/resources/ClientResource.java
+++ backend/src/main/java/com/devricardo/dscatalog/resources/ClientResource.java
@@ -1,5 +1,28 @@
 package com.devricardo.dscatalog.resources;
 
-public class ClientResource {
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devricardo.dscatalog.entities.Client;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+	
+	@GetMapping
+	public ResponseEntity<List<Client>>findAll(){
+		
+		//Instant data = Instant.now();
+		List<Client> list= new ArrayList<>();
+		list.add(new Client(1L, "Maria", "230971fd", 2100.32, Instant.now(), 2 ));
+		list.add(new Client(2L, "Manuel", "230971fe", 2100.32, Instant.now(), 1 ));
+		list.add(new Client(3L, "Joana", "230971re", 2100.32, Instant.now(), 3 ));
+		return ResponseEntity.ok().body(list);
+	}
 }
